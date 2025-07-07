9cd1fd6f470ef38f518a7b2eb1272d93fb5ea675
kavya2510 <kavya251001@gmail.com>
2025-07-08 02:03:12 +0530

    upcoming movies api fix

--- src/hooks/useUpcomingMovies.js
+++ src/hooks/useUpcomingMovies.js
@@ -6,7 +6,7 @@ const useUpcomingMovies = () => {
   const dispatch = useDispatch();
   const getUpcomingMovies = async () => {
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/ucoming?page=1",
+      "https://api.themoviedb.org/3/movie/upcoming?page=1",
       API_OPTIONS
     );
     const json = await data.json();
