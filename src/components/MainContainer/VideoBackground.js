4c3e64d551a01e5b9ebbfe47902fb7e88ac8867f
kavya2510 <kavya251001@gmail.com>
2025-07-07 22:49:29 +0530

    feat: Enhance VideoBackground component to fetch and display trailer videos; update MainContainer to pass movieId

--- src/components/MainContainer/VideoBackground.js
+++ src/components/MainContainer/VideoBackground.js
@@ -1,4 +1,38 @@
-const VideoBackground = () => {
-  return <div>VideoBackground</div>;
+import { API_OPTIONS } from "../../utils/constants";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../../utils/movieSlice";
+
+const VideoBackground = (movieId) => {
+  const dispatch = useDispatch();
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/846422/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    const filteredData = json.results.filter(
+      (video) => video.type === "Trailer"
+    );
+    const trailer = filteredData.length ? filteredData[0] : json.results[0];
+    console.log(trailer);
+    dispatch(addTrailerVideo(trailer));
+  };
+  React.useEffect(() => {
+    getMovieVideos();
+  }, []);
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+      ></iframe>
+    </div>
+  );
 };
 export default VideoBackground;
