28681d43a6a89b7a2b5dcaa01170282da0378cb1
kavya2510 <kavya251001@gmail.com>
2025-07-07 19:54:01 +0530

    feat: Integrate TMDB API to fetch now playing movies and manage state with Redux

--- README.md
+++ README.md
@@ -17,6 +17,8 @@
 - Signout feature
 - Redirection Logic from sign in to browse and vice versa
 - Unsubscribe to onAuthStateChange call back when the component unmounts
+- Register TMDB and create an app and get access token
+- Get Data from tmdb (enable vpn)
 
 
 # Features
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,11 @@ export const LOGO =
   "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-07-01/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const USER_AVATAR =
   "https://occ-0-4857-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMzg0Mzk5MjMyOTcxYjZjMjg0OWNhOGYxMGMxZTUyYSIsIm5iZiI6MTc1MTg5MTE1OS45MjEsInN1YiI6IjY4NmJiY2Q3ZTBkMzY1OTA4MDEwM2I0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5y1-iTgKh6Hwo-kZ_gjrW0dLGQDVIwbyWopmNy__1IE",
+  },
+};
